18e988faee68a1955d6b49962457e54b93cbdbfa
Sufiyan Munshi <101869552+sufiyan02@users.noreply.github.com>
2025-03-06 12:17:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+
+let name = document.getElementById("text")
+let time = document.getElementById("delay")
+let btn = document.getElementById("btn")
+let res = document.getElementById("output")
+btn.addEventListener("click",()=>{
+	let firstVal = name.value
+	let secVal = time.value
+	 setTimeout(()=>{
+
+		  res.innerText = firstVal
+		 
+	 },secVal)
+	
+})
